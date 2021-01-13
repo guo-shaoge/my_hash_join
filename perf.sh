1acf00585e1529f62334de0481de0022507a8437
guo-shaoge <shaoge1994@163.com>
2021-01-13 13:40:55 +0800

    fix perf.sh

--- perf.sh
+++ perf.sh
@@ -5,13 +5,13 @@ make clean
 make -j
 
 echo // 1 thread
-./my_hash_join 1 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_1_f1
+./my_hash_join 1 500 ./dataset/ndv_0_9  ./dataset/ndv_0_1
 
 echo // 2 thread
-./my_hash_join 2 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_1_f1
+./my_hash_join 2 500 ./dataset/ndv_0_9  ./dataset/ndv_0_1
 
 echo // 4 thread
-./my_hash_join 4 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_1_f1
+./my_hash_join 4 500 ./dataset/ndv_0_9  ./dataset/ndv_0_1
 
 echo // 8 thread
-./my_hash_join 8 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_1_f1
+./my_hash_join 8 500 ./dataset/ndv_0_9  ./dataset/ndv_0_1
--- test_ndv.sh
+++ test_ndv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_1_f1
-./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_5_f1
-./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_7_f1
-./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9_f1  ./dataset/ndv_0_9_f1
+./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9  ./dataset/ndv_0_1
+./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9  ./dataset/ndv_0_5
+./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9  ./dataset/ndv_0_7
+./my_hash_join_heavy_key_equal 1 500 ./dataset/ndv_0_9  ./dataset/ndv_1
